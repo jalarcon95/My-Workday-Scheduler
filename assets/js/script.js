4e49c1108a4d8bacbb5a0d87aa258f8dd2a71217
John Alarcon <jalar@MacBook-Pro.attlocal.net>
2023-05-21 14:21:48 -0400

    debugged function and made some spacing changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,57 @@
 const localeSettings = {};
-dayjs.locale(localeSettings);
+  dayjs.locale(localeSettings);
 
-$(function () {
+  $(function () {
 
-  const currentHour = dayjs().format('H');
+    const currentHour = dayjs().format('H');
 
-  function hourlyColor() {
+    function hourlyColor() {
+      $('.time-block').each(function() {
+        const blockHour = parseInt(this.id);
+        $(this).toggleClass('past', blockHour < currentHour);
+        $(this).toggleClass('present', blockHour === currentHour);
+        $(this).toggleClass('future', blockHour > currentHour);
+      });
+    }
+
+    function textEntry() {
+      $('.saveBtn').on('click', function() {
+        const key = $(this).parent().attr('id');
+        const value = $(this).siblings('.description').val();
+        localStorage.setItem(key, value);
+      });
+    }
+    function refreshColor() {
+      $('.time-block').each(function() {
+        const blockHour = parseInt(this.id);
+        if (blockHour == currentHour) {
+          $(this).removeClass('past future').addClass('present');
+        } else if (blockHour < currentHour) {
+          $(this).removeClass('future present').addClass('past');
+        } else {
+          $(this).removeClass('past present').addClass('future');
+        }
+      });
+    }
+
     $('.time-block').each(function() {
-      const blockHour = parseInt(this.id);
-      $(this).toggleClass('past', blockHour < currentHour);
-      $(this).toggleClass('present', blockHour === currentHour);
-      $(this).toggleClass('future', blockHour > currentHour);
+      const key = $(this).attr('id');
+      const value = localStorage.getItem(key);
+      $(this).children('.description').val(value);
     });
-  }
 
-  function textEntry() {
-    $('.saveBtn').on('click', function() {
-      const key = $(this).parent().attr('id');
-      const value = $(this).siblings('description').val();
-      localStorage.setItem(key, value);
-    });
-  }
+    function updateTime() {
+      const dateElement = $('#date');
+      const timeElement = $('#time');
+      const currentDate = dayjs().format('dddd, MMMM D, YYYY');
+      const currentTime = dayjs().format('hh:mm:ss A');
+      dateElement.text(currentDate);
+      timeElement.text(currentTime);
+    }
 
-  function refreshColor() {
-    $('.time-block').each(function() {
-      const blockHour = parseInt(this.id);
-      if (blockHour === currentHour) {
-        $(this).removeClass('past future').addClass('present');
-      } else if(blockHour < currentHour) {
-        $(this).removeClass('future present').addClass('past');
-      } else {
-        $(this).removeClass('past present').addClass('future');
-      }
-   });
-  }
+    hourlyColor();
+    textEntry();
+    refreshColor();
 
-  $('.time-block').each(function() {
-    const key = $(this).attr('id');
-    const value = localStorage.getItem(key);
-    $(this).children('.description').val(value);
+    setInterval(updateTime, 1000);
   });
-
-  function updateTime() {
-    const dateElement = $('#date');
-    const timeElement = $('#time');
-    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
-    const currentTime = dayjs().format('hh:mm:ss A');
-    dateElement.text(currentDate);
-    timeElement.text(currentTime);
-  }
-
-  hourlyColor();
-  textEntry();
-  refreshColor();
-
-  setInterval(updateTime, 1000);
-});
